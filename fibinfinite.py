c511d09ca1ffba3df926f58c024addfb8b8b70c9
codingxlife <63388084+codingxlife@users.noreply.github.com>
2020-04-09 11:04:03 +0530

    Add files via upload

---
+++ fibinfinite.py
@@ -0,0 +1,15 @@
+# Initialization of values
+a, b, i = 0, 1, 1
+
+# n = int(input("How Many Fibonacci Numbers You Want Me to Print?\t"))
+
+# While loop to print fibonacci numbers
+
+while i > 0:            # While loop runs for  n iterations 
+    print (b, '\n')           # Prints calculated fibonacci number
+    a, b = b, a+b       # Replaces values of a and b for next iteration
+
+
+
+
+
